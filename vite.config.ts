11ffa280cc64b29921c512ea7e193867bb0c6514
810505339@qq.com <810505339@qq.com>
2024-04-25 16:07:08 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     UnoCSS(),
